3b50c9e2ed311f12c22486e31a0208784c4a0943
prdpta <pradiptaputramei01@gmail.com>
2024-04-04 18:18:50 +0700

    menerima elemen array

--- AlgorithmaSearch_0037.cpp
+++ AlgorithmaSearch_0037.cpp
@@ -16,3 +16,8 @@ void input()
         else
             cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
     }
+
+    // Accept array elements
+    cout << "\n----------------------\n";
+    cout << " Enter array elements \n";
+    cout << "----------------------\n";
